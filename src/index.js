9b683392a68cfce609444423ddbbfe06e7c0398e
ericpkatz <ericpkatz@gmail.com>
2022-07-23 08:20:51 -0400

    commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
-render(<hr />, document.querySelector('#root'));
+const root = createRoot(document.querySelector('#root'));
+root.render(<hr />);
