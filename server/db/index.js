727135949545fb7fb55ece8cb58dcdc12a86b389
xyshtd <103381676+xyshtd@users.noreply.github.com>
2022-10-06 20:45:57 -0400

    update db name

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/jpfp_student_campus',{logging:false})
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_schools_db',{logging:false})
 
 module.exports = {
   conn
