c03b1383311e261fa7b2a6aaaec97a537ff5f054
xyshtd <mhu225@gmail.com>
2022-10-21 07:24:07 -0400

    404

--- src/components/Global/PageNotFound.js
+++ src/components/Global/PageNotFound.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const PageNotFound = ()=>{
   return (
     <div className = 'pageNotFound'>
-      <h2>PageNotFound</h2>
+      <h2>Oops! Page Not Found</h2>
       <p>Looks like you've followed a broken link or entered a URL that doesn't exist on this site.</p>
       <Link to = '/'>← Back to Homepage</Link>
     </div>
